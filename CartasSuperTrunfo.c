b0fe6a5688574b2ca36dfa84e8ca73dea95e9b2d
Prgexe <tiagofalbuquerque@gmail.com>
2025-01-08 14:43:03 +0000

    tentando fazer comparação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,7 @@ int main() {
     char Codigo;
 
     printf("*** Projeto Cartas Super Trunfos ***\n");
+    printf("PRIMEIRA CARTA:\n");
     printf("Código da cidade\n");
     scanf("%s", &Codigo);
     printf("Número da população\n");
@@ -36,6 +37,65 @@ int main() {
     float pib;
     pib = (float)PIB / Populacao;
     printf("O PIB Per Capita é: %.2f\n", pib);
+
+    //SEGUNDA CARTA
+    int Populacao2;
+    int Area2;
+    int PIB2;
+    int Pontos2;
+    char Codigo2;
+    
+    
+    
+    
+    
+    
+    printf("SEGUNDA CARTA:\n");
+    printf("Código da cidade\n");
+    scanf("%s", &Codigo2);
+    printf("Número da população\n");
+    scanf("%d", &Populacao2);
+    printf("Número da população: %d\n", Populacao2);
+    printf("Área\n");
+    scanf("%d", &Area2);
+    printf("Area: %d\n", Area2);
+    printf("PIB\n");
+    scanf("%d", &PIB2);
+    printf("PIB: %d\n", PIB2);
+    printf("Pontos\n");
+    scanf(" %d", &Pontos2);
+    printf("Pontos: %d\n", Pontos2);
+
+    float densidade2;
+    densidade2 = (float)Populacao2 / Area2;
+    printf("A densidade populacional é: %.2f\n", densidade2);
+
+    float pib2;
+    pib2 = (float)PIB2 / Populacao2;
+    printf("O PIB Per Capita é: %.2f\n", pib2);
+
+    //comparação entre as duas cartas
+    int Populacao2;
+    int Area2;
+    int PIB2;
+    int Pontos2;
+    char Codigo2;
+    int Populacao;
+    int Area;
+    int PIB;
+    int Pontos;
+    char Codigo;
+    
+    
+    
+    
+    printf("densidade2 > densidade: %.2f\n, densidade2 > densidade");
+    printf("pib2 > pib: %.2f\n, pib2 > pib");
+    printf("Pontos2 > Pontos: %d\n, Pontos2 > Pontos");
+    printf("PIB2 > PIB: %d\n, PIB2 > PIB");
+    printf("Area2 > Area: %d\n, Area2 > Area");
+    printf("Populacao2 > Populacao: %d\n, Populacao2 > Populacao");
+
 
 
 
