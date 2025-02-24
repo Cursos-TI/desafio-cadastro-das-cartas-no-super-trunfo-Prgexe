05e9c55a01cf3375fd014911e41fa0fcbe17710d
Prgexe <tiagofalbuquerque@gmail.com>
2025-02-24 13:38:56 +0000

    mestre finalizado, super CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
+
+#include <stdio.h>
 
 int main() {
     int Populacao;
@@ -22,34 +24,27 @@ int main() {
     printf("Número da população: %d\n", Populacao);
     printf("Área\n");
     scanf("%d", &Area);
-    printf("Area: %d\n", Area);
+    printf("Área: %d\n", Area);
     printf("PIB\n");
     scanf("%d", &PIB);
     printf("PIB: %d\n", PIB);
     printf("Pontos\n");
-    scanf(" %d", &Pontos);
+    scanf("%d", &Pontos);
     printf("Pontos: %d\n", Pontos);
 
-    float densidade;
-    densidade = (float)Populacao / Area;
+    float densidade = (float)Populacao / Area;
     printf("A densidade populacional é: %.2f\n", densidade);
 
-    float pib;
-    pib = (float)PIB / Populacao;
+    float pib = (float)PIB / Populacao;
     printf("O PIB Per Capita é: %.2f\n", pib);
 
-    //SEGUNDA CARTA
+    // SEGUNDA CARTA
     int Populacao2;
     int Area2;
     int PIB2;
     int Pontos2;
     char Codigo2;
-    
-    
-    
-    
-    
-    
+
     printf("SEGUNDA CARTA:\n");
     printf("Código da cidade\n");
     scanf("%s", &Codigo2);
@@ -58,44 +53,57 @@ int main() {
     printf("Número da população: %d\n", Populacao2);
     printf("Área\n");
     scanf("%d", &Area2);
-    printf("Area: %d\n", Area2);
+    printf("Área: %d\n", Area2);
     printf("PIB\n");
     scanf("%d", &PIB2);
     printf("PIB: %d\n", PIB2);
     printf("Pontos\n");
-    scanf(" %d", &Pontos2);
+    scanf("%d", &Pontos2);
     printf("Pontos: %d\n", Pontos2);
 
-    float densidade2;
-    densidade2 = (float)Populacao2 / Area2;
+    float densidade2 = (float)Populacao2 / Area2;
     printf("A densidade populacional é: %.2f\n", densidade2);
 
-    float pib2;
-    pib2 = (float)PIB2 / Populacao2;
+    float pib2 = (float)PIB2 / Populacao2;
     printf("O PIB Per Capita é: %.2f\n", pib2);
 
-    //comparação entre as duas cartas
-    int Populacao2;
-    int Area2;
-    int PIB2;
-    int Pontos2;
-    char Codigo2;
-    int Populacao;
-    int Area;
-    int PIB;
-    int Pontos;
-    char Codigo;
-    
-    
-    
-    
-    printf("densidade2 > densidade: %.2f\n, densidade2 > densidade");
-    printf("pib2 > pib: %.2f\n, pib2 > pib");
-    printf("Pontos2 > Pontos: %d\n, Pontos2 > Pontos");
-    printf("PIB2 > PIB: %d\n, PIB2 > PIB");
-    printf("Area2 > Area: %d\n, Area2 > Area");
-    printf("Populacao2 > Populacao: %d\n, Populacao2 > Populacao");
+    // Comparações com operadores relacionais
+    printf("\n*** COMPARAÇÕES ENTRE AS DUAS CARTAS ***\n");
 
+    // Comparando a densidade populacional (menor valor vence)
+    printf("\n*** Densidade Populacional ***\n");
+    if (densidade < densidade2) {
+        printf("A primeira cidade tem a menor densidade populacional e vence.\n");
+    } else if (densidade > densidade2) {
+        printf("A segunda cidade tem a menor densidade populacional e vence.\n");
+    } else {
+        printf("As duas cidades têm a mesma densidade populacional.\n");
+    }
+
+    // Comparando o PIB per capita (maior valor vence)
+    printf("\n*** PIB Per Capita ***\n");
+    if (pib > pib2) {
+        printf("A primeira cidade tem o maior PIB per capita e vence.\n");
+    } else if (pib < pib2) {
+        printf("A segunda cidade tem o maior PIB per capita e vence.\n");
+    } else {
+        printf("As duas cidades têm o mesmo PIB per capita.\n");
+    }
+
+    // Comparando os pontos (maior valor vence)
+    printf("\n*** Pontos ***\n");
+    if (Pontos > Pontos2) {
+        printf("A primeira cidade tem mais pontos e vence.\n");
+    } else if (Pontos < Pontos2) {
+        printf("A segunda cidade tem mais pontos e vence.\n");
+    } else {
+        printf("As duas cidades têm a mesma quantidade de pontos.\n");
+    }
+
+    
+
+
+    
 
 
 
